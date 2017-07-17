379f6a586056ad931c89747d91f000d2e769ebd4
Koo bonik <kbi960130@naver.com>
2017-07-17 23:45:01 +0000

    게시판 데이터베이스 완성

--- WEB-INF/src/inflearn_JSP/inflearn_JSP.java
+++ WEB-INF/src/inflearn_JSP/inflearn_JSP.java
@@ -8,4 +8,40 @@ public class inflearn_JSP{
 	private String inflearn_JSPContent;
 	private int inflearn_JSPAvailable;
 	
+	public int getinflearn_JSPID(){
+		return inflearn_JSPID;
+	}
+	public void setinflearn_JSPID(int inflearn_JSPID){
+		this.inflearn_JSPID = inflearn_JSPID;
+	}
+	public String getinflearn_JSPTitle(){
+		return inflearn_JSPTitle;
+	}
+	publicvoid setinflearn_JSPTitle(String inflearn_JSPTitle){
+		this.inflearn_JSPTitle = inflearn_JSPTitle;
+	}
+	public String getinflearn_JSPID(){
+		return inflearn_JSPID;
+	}
+	public void setinflearn_JSPID(String inflearn_JSPID){
+		this.inflearn_JSPID = inflearn_JSPID;
+	}
+	public String getinflearn_JSPDate(){
+		return inflearn_JSPDate;
+	}
+	public void setinflearn_JSPDate(String inflearn_JSPDate){
+		this.inflearn_JSPDate = inflearn_JSPDate;
+	}
+	public String getinflearn_JSPContent(){
+		return inflearn_JSPContent;
+	}
+	public void setinflearn_JSPContent(){
+		this.inflearn_JSPContent = inflearn_JSPContent;
+	}
+	public int getinflearn_JSPAvailable(){
+		return inflearn_JSPAvailable;
+	}
+	public void setinflearn_JSPAvailable(int inflearn_JSPAvailable){
+		this.inflearn_JSPAvailable = inflearn_JSPAvailable;
+	}
 }
