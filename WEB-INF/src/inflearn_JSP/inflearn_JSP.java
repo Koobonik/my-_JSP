e4b4cb393221cd0d992e0072c8b759ec43aefb0b
Koo bonik <kbi960130@naver.com>
2017-07-17 23:34:04 +0000

    게시판 데이터 베이스 만드는 중...

---
+++ WEB-INF/src/inflearn_JSP/inflearn_JSP.java
@@ -0,0 +1,11 @@
+package inflearn_JSP;
+
+public class inflearn_JSP{
+	
+	private int inflearn_JSPID;
+	private String inflearn_JSPTitle;
+	private String userID;
+	private String inflearn_JSPContent;
+	private int inflearn_JSPAvailable;
+	
+}
