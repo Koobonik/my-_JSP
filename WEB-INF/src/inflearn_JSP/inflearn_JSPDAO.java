b473abe1d388e1cba5cf5ac14ba18b5df4a82f59
Koo bonik <kbi960130@naver.com>
2017-07-20 22:14:21 +0000

    Fix error code and build complete

--- WEB-INF/src/inflearn_JSP/inflearn_JSPDAO.java
+++ WEB-INF/src/inflearn_JSP/inflearn_JSPDAO.java
@@ -70,7 +70,7 @@ public class inflearn_JSPDAO {
 		}
 	public ArrayList<inflearn_JSP> getList(int pageNumber){
 		String SQL = "SELECT * FROM inflearn_JSP WHERE inflearn_JSPID < ? AND inflearn_JSPIDAvailable = 1 ORDER BY inflearn_JSPIDDESC LIMIT 10";
-		ArrayList<inflearn_JSP> list = new inflearn_JSP<inflearn_JSP()>;
+		ArrayList<inflearn_JSP> list = new ArrayList<inflearn_JSP>();
 			try{
 				PreparedStatement pstmt = conn.prepareStatement(SQL);
 				pstmt.setInt(1, getNext() - (pageNumber -1 ) * 10);
@@ -82,8 +82,8 @@ public class inflearn_JSPDAO {
 					Inflearn_JSP.setUserID(rs.getString(3));
 					Inflearn_JSP.setinflearn_JSPDate(rs.getString(4));
 					Inflearn_JSP.setinflearn_JSPContent(rs.getString(5));
-					Inflearn_JSP.setinflearn_JSPAvaliable(rs.getInt(6));
-					list.add(inflearn_JSP);
+					Inflearn_JSP.setinflearn_JSPAvailable(rs.getInt(6));
+					list.add(Inflearn_JSP);
 				}
 			}catch(Exception e){
 				e.printStackTrace();
@@ -104,5 +104,39 @@ public class inflearn_JSPDAO {
 			}
 			return false;
 	}
-	
+	public inflearn_JSP getinflearn_JSP(int inflearn_JSPID){
+		String SQL = "SELECT * FROM BBS WHERE inflearn_JSPID = ?";
+		try{
+			PreparedStatement pstmt = conn.prepareStatement(SQL);
+			pstmt.setInt(1, inflearn_JSPID);
+			rs = pstmt.executeQuery();
+			if(rs.next()){
+				inflearn_JSP Inflearn_JSP = new inflearn_JSP();
+				Inflearn_JSP.setinflearn_JSPID(rs.getInt(1));
+				Inflearn_JSP.setinflearn_JSPTitle(rs.getString(2));
+				Inflearn_JSP.setUserID(rs.getString(3));
+				Inflearn_JSP.setinflearn_JSPDate(rs.getString(4));
+				Inflearn_JSP.setinflearn_JSPContent(rs.getString(5));
+				Inflearn_JSP.setinflearn_JSPAvailable(rs.getInt(6));
+				return Inflearn_JSP;
+			}
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public int update(int inflearn_JSPID, String inflearn_JSPTitle, String inflearn_JSPContent){
+		String SQL = "UPDATE INFLEARN_JSP SET inflearn_JSPTitle = ?, inflearn_JSPContent = ? WHERE inflearn_JSPID = ?";
+			try{
+				PreparedStatement pstmt = conn.prepareStatement(SQL);
+				pstmt.setString(1, inflearn_JSPTitle);
+				pstmt.setString(2, inflearn_JSPContent);
+				pstmt.setInt(6, inflearn_JSPID);
+				return pstmt.executeUpdate();
+			
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			return -1;//데이터 베이스오류
+	}
 }
